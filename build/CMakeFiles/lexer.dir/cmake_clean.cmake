d1d8d84a5718c81651ae58dc3df29aba654545f3
Maria K <klimenteva.mr@phystech.edu>
2024-01-28 15:58:51 +0300

    simplest lexer version

---
+++ build/CMakeFiles/lexer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lexer.dir/lexer.cc.o"
+  "CMakeFiles/lexer.dir/numlex.cc.o"
+  "lexer"
+  "lexer.cc"
+  "lexer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lexer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
